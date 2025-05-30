b95eb6dd0ee5404be8067fbc9fcfb672c45e3d86
itamm15 <osinski.mateusz15@gmail.com>
2025-05-30 10:46:05 +0200

    init commit

---
+++ lib/ex_synodse.ex
@@ -0,0 +1,18 @@
+defmodule ExSynodse do
+  @moduledoc """
+  Documentation for `ExSynodse`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSynodse.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_synodse_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSynodseTest do
+  use ExUnit.Case
+  doctest ExSynodse
+
+  test "greets the world" do
+    assert ExSynodse.hello() == :world
+  end
+end
