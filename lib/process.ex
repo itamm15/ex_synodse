ae92e5d85aea4217bd4ac93e6bdb33261702107c
itamm15 <osinski.mateusz15@gmail.com>
2025-06-12 10:39:04 +0200

    chore: raise error when sgp is restartable

--- lib/process.ex
+++ lib/process.ex
@@ -6,6 +6,10 @@ defmodule SupervisedProcess do
     sgp? = Keyword.get(args, :sgp?, false)
     restart? = Keyword.get(args, :restart?, false)
 
+    if sgp? and restart? do
+      raise "sgp? and restart? cannot be true at the same time"
+    end
+
     %__MODULE__{module: module, sgp?: sgp?, restart?: restart?}
   end
 end
