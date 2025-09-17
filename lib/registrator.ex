f88c18718f017570b38d5c526e07b9da598e60c3
itamm15 <osinski.mateusz15@gmail.com>
2025-09-17 12:10:01 +0200

    chore: handle net split when joining cluster

--- lib/registrator.ex
+++ lib/registrator.ex
@@ -7,7 +7,7 @@ defmodule ExSynodse.Registrator do
   require Logger
 
   @enforce_keys [:supervisor]
-  defstruct [:supervisor, processes: []]
+  defstruct [:supervisor, processes: [], cluster_join_retries: 0]
 
   def new(processes) do
     processes = Enum.map(processes, &ExSynodse.SupervisedProcess.new(&1))
@@ -35,10 +35,19 @@ defmodule ExSynodse.Registrator do
   end
 
   @impl true
+  def handle_info(:check_if_connected_to_cluster, state) do
+    leader =  :global.whereis_name(:leader)
+
+    ## TODO: add health check for the leader (by database)
+
+    maybe_monitor_and_notify_leader(leader, state)
+  end
+
+  @impl true
   def handle_info({:DOWN, _ref, :process, exited_pid, reason}, state) do
     Logger.info("Node #{inspect(exited_pid)} is down, reason: #{inspect(reason)}")
 
-    ## todo; handle leader election in case the leader is down
+    ## todo; handle leader election in case the leader is down + register again :global.register_name(:leader, self())
     processes_to_restart = Enum.filter(state.processes, & &1.restart?)
     supervise_processes(processes_to_restart, state.supervisor)
 
@@ -46,24 +55,17 @@ defmodule ExSynodse.Registrator do
   end
 
   defp register(%__MODULE__{} = state) do
-    node = self()
-
     case try_to_become_leader() do
       {:ok, _leader} ->
-        Logger.info("I am the leader, #{inspect(node)}")
+        Logger.info("I am the leader, #{inspect(self())}")
         supervise_processes(state.processes, state.supervisor)
+        :global.register_name(:leader, self())
 
       {:error, error} ->
+        Logger.warning("Failed to become leader, #{inspect(error)}")
+
         leader = :global.whereis_name(:leader)
-
-        Logger.info(
-          "I am not the leader, I will monitor the leader, #{inspect(leader)}, #{inspect(error)}"
-        )
-
-        ## notify the leader about new node
-        send(leader, {:monitor_me, node})
-        ## monitor the leader
-        monitor_node(leader)
+        maybe_monitor_and_notify_leader(leader, state)
     end
 
     state
@@ -110,6 +112,23 @@ defmodule ExSynodse.Registrator do
 
       Supervisor.start_child(supervisor, module)
     end)
+  end
+
+  defp maybe_monitor_and_notify_leader(:undefined, state) do
+    Logger.warning("Leader is undefined - #{inspect(Node.self())} is probably not connected to the cluster, retrying #{state.cluster_join_retries} times")
+
+    Process.send_after(self(), :check_if_connected_to_cluster, 1000)
+
+    state = %{state | cluster_join_retries: state.cluster_join_retries + 1}
+
+    {:noreply, state}
+  end
+
+  defp maybe_monitor_and_notify_leader(leader, state) do
+    Logger.info("Leader is #{inspect(leader)}")
+    send(leader, {:monitor_me, self()})
+    monitor_node(leader)
+    {:noreply, state}
   end
 
   defp start_leader_supervisor do
